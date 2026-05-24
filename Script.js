3346416379ab2876d6ebfdae2befab59da3b8a22
pulga100mil <fcfykhr689@privaterelay.appleid.com>
2026-05-23 21:59:09 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,12 +3,19 @@ let texto = document.getElementById("input").value;
 let respuesta = document.getElementById("respuesta");
 
 if(texto.toLowerCase().includes("hola")){
-respuesta.innerText = "Hola 👋 ¿qué necesitas?";
-}
-else if(texto.toLowerCase().includes("quien eres")){
-respuesta.innerText = "Soy tu IA hecha en GitHub Pages 🤖";
+respuesta.innerText = "Hola 👋 ya puedes subir imágenes también";
 }
 else{
-respuesta.innerText = "No entiendo eso todavía 😅";
+respuesta.innerText = "Recibí tu mensaje 👍";
 }
+}
+
+// Mostrar imagen
+function cargarImagen(event){
+let img = document.getElementById("preview");
+img.src = URL.createObjectURL(event.target.files[0]);
+img.style.display = "block";
+
+document.getElementById("respuesta").innerText =
+"Imagen cargada 📸 (la IA aún no la analiza)";
 }
