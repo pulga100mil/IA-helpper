9ea7f33afb66788699ded05986c10ec66048767b
pulga100mil <fcfykhr689@privaterelay.appleid.com>
2026-05-23 21:49:26 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
-console.log("Hola");
+function responder(){
+let texto = document.getElementById("input").value;
+let respuesta = document.getElementById("respuesta");
+
+if(texto.toLowerCase().includes("hola")){
+respuesta.innerText = "Hola 👋 ¿qué necesitas?";
+}
+else if(texto.toLowerCase().includes("quien eres")){
+respuesta.innerText = "Soy tu IA hecha en GitHub Pages 🤖";
+}
+else{
+respuesta.innerText = "No entiendo eso todavía 😅";
+}
+}
