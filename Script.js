6817fbb93e649e63b1b456dd91de4fb001644906
pulga100mil <fcfykhr689@privaterelay.appleid.com>
2026-05-23 22:04:40 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
+window.onload = function(){
+
 setTimeout(function(){
 
 document.getElementById("inicio").style.display="none";
@@ -6,12 +8,13 @@ document.getElementById("chat").style.display="block";
 
 },3000);
 
+}
 
 function responder(){
 
 let texto=document.getElementById("input").value;
 let respuesta=document.getElementById("respuesta");
 
-respuesta.innerText="Pulga IA recibió: "+texto;
+respuesta.innerText="Pulga IA recibió: " + texto;
 
 }
